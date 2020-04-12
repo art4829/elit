812352f895260702f4ce21d8b46ef035555e685e
mt2915 <mt2915@g.rit.edu>
2020-04-11 18:21:12 -0700

    Made users.plist an array. Edited Login and Signup

--- elit/FirstViewController.swift
+++ elit/FirstViewController.swift
@@ -18,3 +18,23 @@ class FirstViewController: UIViewController {
 
 }
 
+//{
+//  "popularity": 16.208,
+//  "vote_count": 2162,
+//  "video": false,
+//  "poster_path": "/7vR4e3ga3mdSEpYe8kMeUv3mgND.jpg",
+//  "id": 9820,
+//  "adult": false,
+//  "backdrop_path": "/3bO3b2wYCANInuzzvTlwXSsJT3N.jpg",
+//  "original_language": "en",
+//  "original_title": "The Parent Trap",
+//  "genre_ids": [
+//    35,
+//    10751
+//  ],
+//  "title": "The Parent Trap",
+//  "vote_average": 7,
+//  "overview": "Hallie Parker and Annie James are identical twins separated at a young age because of their parents' divorce. unknowingly to their parents, the girls are sent to the same summer camp where they meet, discover the truth about themselves, and then plot with each other to switch places. Hallie meets her mother, and Annie meets her father for the first time in years.",
+//  "release_date": "1998-07-28"
+//}
+
--- elit/LoginViewController.swift
+++ elit/LoginViewController.swift
@@ -13,53 +13,46 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameEntered: UITextField!
     @IBOutlet weak var passwordEntered: UITextField!
     
+    var usersList = Users()
     var username = ""
     var password = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
-        if let documentsPathURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            var plistPath = documentsPathURL.appendingPathComponent("user.plist").path
-            if !FileManager.default.fileExists(atPath: plistPath) {
-                plistPath = Bundle.main.path(forResource: "user", ofType: "plist")!
-            }
-            
-            //Retrieve the username and password
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: plistPath))
-                
-                print(data)
-                let temp = try PropertyListSerialization.propertyList(from: data, options: .mutableContainersAndLeaves, format: nil) as! [String:Any]
-                
-                print("\(temp)")
-                
-                username = temp["username"] as? String ?? "TBD"
-                password = temp["password"] as? String ?? "TBD"
-                
-            } catch {
-                print(error)
-            }
-        }
+        usersList.userList = globalUsersList
+        username = ""
+        password = ""
     }
 
-    
     @IBAction func login(sender: UIButton) {
             
         //Check if the username and password are correct
-        if (username == usernameEntered.text! && password == passwordEntered.text!) {
-            performSegue(withIdentifier: "LoginToHome", sender: self)
+        if (usernameEntered.text! == "" || passwordEntered.text! == "") {
+            alertUser(message: "Please put in all fields")
+            return
+        }
+        for user in globalUsersList {
+            if (user.getUsername() == usernameEntered.text! && user.getPassword() == passwordEntered.text!) {
+                username = user.getUsername();
+                password = user.getPassword();
+            }
+        }
+        if (username != "" && password != "") {
+             performSegue(withIdentifier: "LoginToHome", sender: self)
         } else {
-            let alertController = UIAlertController(title: "Error", message: "Incorrect Username or Password", preferredStyle: .alert)
-            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-             alertController.addAction(defaultAction)
-             self.present(alertController, animated: true, completion: nil)
+            alertUser(message: "Incorrect Username or Password")
         }
         
         view.endEditing(true) //dismiss the keyboard whenever you have multiple fields
     }
     
+    func alertUser(message : String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+         alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
     
     @IBAction func textFieldReturn(sender: UITextField) {
         sender.resignFirstResponder()
